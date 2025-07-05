51325127ae18d8a3b5a9692837bf7e783efd2c47
Nikhila-38 <pagadala.nikhila.3891@gmail.com>
2025-07-05 22:40:23 +0530

    30th June

---
+++ src/ex_17_OOPS/Lab166_OOPs.java
@@ -0,0 +1,8 @@
+package ex_17_OOPS;
+
+public class Lab166_OOPs {
+    public static void main(String[] args){
+        Person amit = new Person();
+        Person arun = new Person();
+    }
+}
---
+++ src/ex_17_OOPS/Person.java
@@ -0,0 +1,24 @@
+package ex_17_OOPS;
+
+public class Person {
+    public static void main{
+        String name;
+        int age;
+        String height;
+        String gender;
+        boolean is_married;
+
+        void walk(){
+            System.out.println("Walking");
+        }
+        void eat(){
+            System.out.println("Eating");
+        }
+        void sleep(){
+            System.out.println("Sleeping");
+        }
+        String food(int a){
+            System.out.println("Walking");
+        }
+    }
+}
