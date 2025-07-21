202e47fcbc3007b152040146858233f4cad32bf8
Nikhila-38 <pagadala.nikhila.3891@gmail.com>
2025-07-21 20:41:13 +0530

    21 July 2025.

---
+++ src/ex_01_Java_Basic/tester_push.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class tester_push {
+    public static void main(String[] args) {
+        String a = "Dev";
+        String b = "Saranya";
+
+        String Result = a + b;
+        System.out.println(Result);
+    }
+}
