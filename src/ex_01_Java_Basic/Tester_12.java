c1e4eda2c7253e860752390ec939631b7c660575
Nikhila-38 <pagadala.nikhila.3891@gmail.com>
2025-07-21 20:23:23 +0530

    21 July 2025.

---
+++ src/ex_01_Java_Basic/Tester_12.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Tester_12 {
+    public static void main(String[] args) {
+        String a = "Danush";
+        String b = "Shreya";
+
+        String Result = a + b;
+        System.out.println(Result);
+    }
+}
