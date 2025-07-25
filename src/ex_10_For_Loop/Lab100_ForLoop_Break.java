3311ca0b06aec7f7e64e3b06341d30e3b4e959d0
Nikhila-38 <pagadala.nikhila.3891@gmail.com>
2025-07-26 01:03:50 +0530

    26 July 2025.

---
+++ src/ex_10_For_Loop/Lab100_ForLoop_Break.java
@@ -0,0 +1,18 @@
+package ex_10_For_Loop;
+
+public class Lab100_ForLoop_Break {
+
+    public static void main(String[] args) {
+        //if loop cannot have break statement but for loop can have break
+
+       /* if (int i =0; i<50; i++) { // 0-49 , 50 times
+            if (i == 5) {
+                break;
+            }
+            else {
+                System.out.println(i);
+
+            }
+              }*/
+    }
+}
